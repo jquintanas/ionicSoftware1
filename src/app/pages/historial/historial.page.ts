2f3d3d2b0f23bc48ad925324909a1191ebba08bc
franmanging <f_manging95@hotmail.com>
2020-07-03 13:17:20 -0500

    UPADTE, refactoring telefono + historial

--- src/app/pages/historial/historial.page.ts
+++ src/app/pages/historial/historial.page.ts
@@ -9,11 +9,10 @@ import {BehaviorSubject} from 'rxjs';
   templateUrl: './historial.page.html',
   styleUrls: ['./historial.page.scss'],
 })
-export class HistorialPage implements OnInit {
 
-//TEMPORIZADOR
-time: BehaviorSubject<string> = new BehaviorSubject ('00');
-timer: number;
+export class HistorialPage implements OnInit {
+  time: BehaviorSubject<string>= new BehaviorSubject ('00');
+  timer: number;
 
 dataHistorial: detalleHistorial[]=[
     {
@@ -46,35 +45,30 @@ dataHistorial: detalleHistorial[]=[
 dataMostrar: any[];
 segment: string = "active";
 
-  constructor(private alertController: AlertController, private router: Router) { 
+  constructor(
+    private alertController: AlertController, 
+    private router: Router) { 
     this.dataMostrar = this.dataHistorial;
   }
 
   ngOnInit() {}
 
-    /*
-      Fecha de Creación: 25/05/2020
-      Fecha de Modificación: 25/05/2020
-      Usuario de creación: Fman
-      Usuario de Modificación: Fman
-      Alert para cancelar pedido
-    */
     async cancelAlert(){
       const alert = await this.alertController.create({
         cssClass: 'alertCancel',
         header: 'Cancelar Pedido',
         inputs : [
           {
-            type:'radio',
-            name:'motivo',
-            label:'Pedido equivocado',
-            value:'pedidoEquivocad'
+            type: 'radio',
+            name: 'motivo',
+            label: 'Pedido equivocado',
+            value: 'pedidoEquivocad'
           },
           {
-            type:'radio',
+            type: 'radio',
             name: 'motivo',
-            label:'Repartidor demorado',
-            value:'repDemorado'
+            label: 'Repartidor demorado',
+            value: 'repDemorado'
           }
           ],
         buttons: [
@@ -94,17 +88,10 @@ segment: string = "active";
       await alert.present();
     }
   
-    /*
-      Fecha de Creación: 25/05/2020
-      Fecha de Modificación: 25/05/2020
-      Usuario de creación: Fman
-      Usuario de Modificación: Fman
-      Alert para ingresar el motivo de la cancelacion del producto
-    */
     async motiveAlert(){
       const alert = await this.alertController.create({
         header: 'Motivo',
-        inputs : [
+        inputs: [
           {
             name: 'name1',
             type: 'text',
@@ -121,39 +108,17 @@ segment: string = "active";
       await alert.present();
     }
 
-    /*
-      Fecha de Creación: 25/05/2020
-      Fecha de Modificación: 25/05/2020
-      Usuario de creación: Fman
-      Usuario de Modificación: Fman
-      Redireccion a la pagina del Carrito de compras
-    */
     goCarrito(){
       this.router.navigateByUrl('carrito-compras');
     }
 
-    /*
-      Fecha de Creación: 25/05/2020
-      Fecha de Modificación: 25/05/2020
-      Usuario de creación: Fman
-      Usuario de Modificación: Fman
-      Iniciar el temporizador 
-      Parámetro de entrada: number
-    */
     startTimer(duration: number){
-      this.timer= duration * 60;
+      this.timer = duration * 60;
       setInterval( () => {
         this.updateTimeValue();
       }, 1000);
     }
-  
-    /*
-      Fecha de Creación: 25/05/2020
-      Fecha de Modificación: 25/05/2020
-      Usuario de creación: Fman
-      Usuario de Modificación: Fman
-      Activacion para que el temporizador funcione
-    */
+
     updateTimeValue(){
       let minutes: any = this.timer /60;
       let seconds: any = this.timer %60;
--- src/app/pages/telefono/telefono.page.ts
+++ src/app/pages/telefono/telefono.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { FirebaseAuthentication } from '@ionic-native/firebase-authentication/ngx';
-import { AlertController} from '@ionic/angular'
+import { AlertController } from '@ionic/angular'
 
 @Component({
   selector: 'app-telefono',
@@ -36,47 +36,44 @@ export class TelefonoPage implements OnInit {
     ]
   }
 
-
-  constructor(private formBuilder: FormBuilder, private firebaseAuthentication: FirebaseAuthentication, public alertController: AlertController) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private firebaseAuthentication: FirebaseAuthentication,
+    public alertController: AlertController) { }
 
   ngOnInit() { }
-
-
 
   askCode() {
     this.firebaseAuthentication.verifyPhoneNumber('+593' + this.phoneNumber, 30000).then(verificationId => {
       alert(verificationId);
-     // this.validateAlert(verificationId)
     })
   }
 
-  // async validateAlert(verificationId) 
   async validateAlert() {
-      const alert = await this.alertController.create({
-        header: 'Código de Verificación',
-        inputs:[
-          {
-            name: 'code',
-            type: 'number',
-            placeholder: 'Ingresa tu código'
-          }
-        ],
-        buttons: [
-          {
-            text: 'Cancelar',
-            role: 'cancel',
-            handler: () => {}
-          }, 
-          {
-            text: 'Verificar',
-            handler: (response) => {
-              const smsCode = response.code;
-              //this.firebaseAuthentication.signInWithVerificationId(verificationId,smsCode).then(res => alert(response))
-            }
-          }
-        ]
-      });
-      await alert.present();
+    const alert = await this.alertController.create({
+      header: 'Código de Verificación',
+      inputs: [
+        {
+          name: 'code',
+          type: 'number',
+          placeholder: 'Ingresa tu código'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => { }
+        },
+        {
+          text: 'Verificar',
+          handler: (response) => {
+            const smsCode = response.code;
+           }
+        }
+      ]
+    });
+    await alert.present();
   }
 
 }
