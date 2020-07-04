b9d8b3757e0a9c84cd16e4c40633ec1a81a3bae7
drios96 <dprios@espol.edu.ec>
2020-07-03 20:34:47 -0500

    UPDATE, refactoring para página recuperar contraseña.

--- src/app/pages/recuperar-contrasena/recuperar-contrasena.page.ts
+++ src/app/pages/recuperar-contrasena/recuperar-contrasena.page.ts
@@ -1,31 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup,Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { ModalController, NavController } from '@ionic/angular';
-import { AlertsService } from 'src/app/services/alerts/alerts.service';
-AlertsService
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ModalController, NavController } from "@ionic/angular";
+import { AlertsService } from "src/app/services/alerts/alerts.service";
+import { environment } from "./../../../environments/environment";
+AlertsService;
 @Component({
-  selector: 'app-recuperar-contrasena',
-  templateUrl: './recuperar-contrasena.page.html',
-  styleUrls: ['./recuperar-contrasena.page.scss'],
+  selector: "app-recuperar-contrasena",
+  templateUrl: "./recuperar-contrasena.page.html",
+  styleUrls: ["./recuperar-contrasena.page.scss"],
 })
 export class RecuperarContrasenaPage implements OnInit {
-
-  private phonepattern : any = /^(09){1}[0-9]{8}$/;
-  formulario_r : FormGroup;
-  private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  constructor(private formBuilder: FormBuilder, private router: Router,private modalController: ModalController,public alertsService: AlertsService) { 
+  recoveryForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private modalController: ModalController,
+    public alertsService: AlertsService
+  ) {
     this.buildForm();
   }
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
 
-  buildForm(){
-    this.formulario_r =this.formBuilder.group({
-      //telefono: ['',[Validators.required,Validators.maxLength(10),Validators.minLength(10), Validators.pattern(this.phonepattern)]],
-      correo: ['',[Validators.required,Validators.pattern(this.emailPattern)]],
+  buildForm() {
+    this.recoveryForm = this.formBuilder.group({
+      emailField: [
+        "",
+        [Validators.required, Validators.pattern(environment.emailPatter)],
+      ],
     });
   }
 
@@ -33,33 +36,36 @@ export class RecuperarContrasenaPage implements OnInit {
     this.modalController.dismiss();
   }
   save() {
-    if (this.formulario_r.valid) {
-      const value = this.formulario_r.value;
+    if (this.recoveryForm.valid) {
+      const value = this.recoveryForm.value;
       console.log(value);
-      console.log(this.formulario_r);
+      console.log(this.recoveryForm);
       this.alertsService.presentLoading("Enviando Mensaje");
-    }else{
-      console.log('formulario inválido',this.formulario_r);
-      //this.isSubmitted = true;
+    } else {
+      console.log("formulario inválido", this.recoveryForm);
       this.onResetForm();
-      
     }
   }
 
-
-  get telefono(){
-    return this.formulario_r.get('telefono');
+  onResetForm() {
+    this.recoveryForm.reset();
   }
 
-  get correo(){
-    return this.formulario_r.get('correo');
-  }
-
-  onResetForm(){
-    this.formulario_r.reset();
-  }
-
-  regresar(){
-    this.router.navigateByUrl('login');
+  public getError(controlName: string): string {
+    let control = this.recoveryForm.get(controlName);
+    let field;
+    if ((control.touched || control.dirty) && control.errors != null) {
+      if (control.errors.required != null) {
+        field = controlName;
+        if (controlName == "emailField") {
+          field = "Correo";
+        }
+        return "El campo " + field + " es requerido.";
+      }
+      if (controlName == "emailField" && control.errors.pattern != null) {
+        return "Ingrese un correo electrónico válido";
+      }
+    }
+    return "";
   }
 }
