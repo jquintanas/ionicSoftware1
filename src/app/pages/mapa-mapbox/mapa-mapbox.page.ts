070082038d234e56967bb6fb311784b94796d613
drios96 <dprios@espol.edu.ec>
2020-07-03 22:03:35 -0500

    UPDATE, refactoring a página mapa mapbox.

--- src/app/pages/mapa-mapbox/mapa-mapbox.page.ts
+++ src/app/pages/mapa-mapbox/mapa-mapbox.page.ts
@@ -1,168 +1,160 @@
-import { AlertsService } from 'src/app/services/alerts/alerts.service';
-import { MapaDatosService } from './../../services/mapa-datos/mapa-datos.service';
-import { Component, OnInit } from '@angular/core';
-import * as Mapboxgl from 'mapbox-gl';
-import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-import { environment} from './../../../environments/environment';
-import { ModalController, NavController } from '@ionic/angular';
-import { AlertController } from '@ionic/angular';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { AlertsService } from "src/app/services/alerts/alerts.service";
+import { MapaDatosService } from "./../../services/mapa-datos/mapa-datos.service";
+import { Component, OnInit } from "@angular/core";
+import * as Mapboxgl from "mapbox-gl";
+import * as MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import { environment } from "./../../../environments/environment";
+import { ModalController, NavController } from "@ionic/angular";
+import { AlertController } from "@ionic/angular";
+import { Geolocation } from "@ionic-native/geolocation/ngx";
 
 @Component({
-  selector: 'app-mapa-mapbox',
-  templateUrl: './mapa-mapbox.page.html',
-  styleUrls: ['./mapa-mapbox.page.scss'],
+  selector: "app-mapa-mapbox",
+  templateUrl: "./mapa-mapbox.page.html",
+  styleUrls: ["./mapa-mapbox.page.scss"],
 })
 export class MapaMapboxPage implements OnInit {
   mapa: Mapboxgl.Map;
   element: any;
   latitud: number;
-  longitud : number;
+  longitud: number;
   latitudCentro: number;
   longitudCentro: number;
   nuevoMarcador: Mapboxgl.Marker;
   guardar = true;
   posicion: string = "";
   reverseGeocode: MapboxGeocoder;
-  omipaliubi= false;
+  omipaliubi = false;
   botonGuardarnuevo = true;
   direccionDom = true;
-  constructor(private modalController: ModalController, public alertController: AlertController, private mapaDatosService: MapaDatosService,public geolocation: Geolocation, public alertsService: AlertsService, public navCtrl: NavController) {
-    //console.log("constructor: ",this.mapaDatosService.NuevaUbicacion );
-    if(this.mapaDatosService.NuevaUbicacion == false){
+  constructor(
+    private modalController: ModalController,
+    public alertController: AlertController,
+    private mapaDatosService: MapaDatosService,
+    public geolocation: Geolocation,
+    public alertsService: AlertsService,
+    public navCtrl: NavController
+  ) {
+    if (this.mapaDatosService.NuevaUbicacion == false) {
       this.omipaliubi = true;
       this.botonGuardarnuevo = false;
-      this.direccionDom = false
+      this.direccionDom = false;
     }
-   }
-
-  ionViewWillEnter(){
   }
 
+  ionViewWillEnter() {}
+
   ngOnInit() {
-    this.geolocation.getCurrentPosition().then((resp) => {       
-        if(this.mapaDatosService.NuevaUbicacion == false){
+    this.geolocation
+      .getCurrentPosition()
+      .then((resp) => {
+        if (this.mapaDatosService.NuevaUbicacion == false) {
           this.latitudCentro = -1.8025916;
           this.longitudCentro = -79.539615;
-        }else{
+        } else {
           this.latitudCentro = resp.coords.latitude;
           this.longitudCentro = resp.coords.longitude;
         }
         (Mapboxgl as any).accessToken = environment.mapboxkey;
         this.mapa = new Mapboxgl.Map({
-          container: 'mapa', // container id
-          style: 'mapbox://styles/mapbox/streets-v11',
-          //center:[-79.5419038,-1.8017518],
-          center: [ this.longitudCentro,this.latitudCentro], // coordenadas del usuario
-          zoom: 13 // starting zoom
+          container: "mapa", 
+          style: "mapbox://styles/mapbox/streets-v11",
+          center: [this.longitudCentro, this.latitudCentro], 
+          zoom: 13, 
         });
-  
-        this.mapa.on('load', ()=> {
-          this.mapa.resize();        
-          });
-        /*
-          Posicion del local OmiPali
-        */
-        const marker_omipali = new Mapboxgl.Marker({draggable: false,color: 'red'})     
-          .setLngLat([-79.5419038,-1.8017518])
+
+        this.mapa.on("load", () => {
+          this.mapa.resize();
+        });
+
+        const marker_omipali = new Mapboxgl.Marker({
+          draggable: false,
+          color: "red",
+        })
+          .setLngLat([-79.5419038, -1.8017518])
           .setPopup(
-            new Mapboxgl.Popup({ offset: 30 }) // add popups
-              .setHTML('<h1> <STRONG>Omi&Pali</STRONG> </h1>'+
-                        '<hr color="black">'+
-                        '<p>'+'<STRONG>Pastelería </STRONG>'+'<style> h1 { color: #FF0000; font-size: 1rem}</style>')              
+            new Mapboxgl.Popup({ offset: 30 })
+              .setHTML(
+                "<h1> <STRONG>Omi&Pali</STRONG> </h1>" +
+                  '<hr color="black">' +
+                  "<p>" +
+                  "<STRONG>Pastelería </STRONG>" +
+                  "<style> h1 { color: #FF0000; font-size: 1rem}</style>"
+              )
           )
           .addTo(this.mapa);
-  
         //marcador en ubicacion de usuario
-        const marker_user= new Mapboxgl.Marker({draggable: true,color: 'orange'})     
-        .setLngLat([resp.coords.longitude,resp.coords.latitude])
-        .setPopup(
-          new Mapboxgl.Popup({ offset: 30 }) // add popups
-            .setHTML('<body>'+ 
-                        '<div class="title">'+
-                          '<h3> Información </h3>'+
-                          '<hr color="black">'+
-                          '<p> Este marcador indica su ubicación </p>'+
-                        '</div>'+
-                      '</body>'+
-                      '<style> h3 { color: orange; font-size: 1rem}</style>'                
-                    ))
-        .addTo(this.mapa);
-        marker_user.on('dragend',()=>{
+        const marker_user = new Mapboxgl.Marker({
+          draggable: true,
+          color: "orange",
+        })
+          .setLngLat([resp.coords.longitude, resp.coords.latitude])
+          .setPopup(
+            new Mapboxgl.Popup({ offset: 30 })
+              .setHTML(
+                "<body>" +
+                  '<div class="title">' +
+                  "<h3> Información </h3>" +
+                  '<hr color="black">' +
+                  "<p> Este marcador indica su ubicación </p>" +
+                  "</div>" +
+                  "</body>" +
+                  "<style> h3 { color: orange; font-size: 1rem}</style>"
+              )
+          )
+          .addTo(this.mapa);
+        marker_user.on("dragend", () => {
           var lngLat = marker_user.getLngLat();
           this.latitud = lngLat.lat;
           this.longitud = lngLat.lng;
-  
-        })
+        });
         this.latitud = marker_user.getLngLat().lat;
         this.longitud = marker_user.getLngLat().lng;
-        //console.log(marker_user.getLngLat().lat);
-  
-        
-     
-    
+
         const geocoder = new MapboxGeocoder({
           accessToken: Mapboxgl.accessToken,
-          countries: 'ec',
+          countries: "ec",
           mapboxgl: Mapboxgl,
           zoom: 15,
-          marker: {color:'blue'},
+          marker: { color: "blue" },
           placeholder: "Buscar",
-          language: 'es-EC',
-          
-        });    
+          language: "es-EC",
+        });
         this.mapa.addControl(geocoder);
-       const geolocalizacion = new Mapboxgl.GeolocateControl({
-          positionOptions: {enableHighAccuracy: true},
-          trackUserLocation: true,      
+        const geolocalizacion = new Mapboxgl.GeolocateControl({
+          positionOptions: { enableHighAccuracy: true },
+          trackUserLocation: true,
         });
         this.mapa.addControl(geolocalizacion);
-      
-
-     }).catch((error) => {
-       console.log('Error al capturar su ubicación', error);
-    });
-    
-    
+      })
+      .catch((error) => {
+        console.log("Error al capturar su ubicación", error);
+      });
   }
 
-
-  async guardar_marcador(){
-      //console.log('esta es la posicion del marcador',this.latitud +' ' + this.longitud);
-      this.mapaDatosService.latitud = this.latitud;
-      this.mapaDatosService.longitud = this.longitud;
-      this.mapaDatosService.marcador_guardado = true;
-      //console.log('la posicion del marcador en lat: '+this.mapaDatosService.latitud);
-      //console.log('la posicion del marcador en lng: '+this.mapaDatosService.longitud);
-      this.posicion = this.latitud.toString() +"|"+this.longitud.toString();
-      //this.marcadorGuardado();
+  async saveMarker() {
+    this.mapaDatosService.latitud = this.latitud;
+    this.mapaDatosService.longitud = this.longitud;
+    this.mapaDatosService.marcador_guardado = true;
+    this.posicion = this.latitud.toString() + "|" + this.longitud.toString();
+    if(this.botonGuardarnuevo == true){
+      console.log("es true");
       this.alertsService.presentToast("Posición guardada");
-      await this.modalController.dismiss(this.posicion);
-
+    }
+    await this.modalController.dismiss(this.posicion);
   }
 
-  
-  async marcadorGuardado() {
+  async markerSuccessfully() {
     const alert = await this.alertController.create({
-      header: 'Guardado exitoso',
-      message: '<hr>'+'<STRONG>La ubicación se guardó con éxito. </strong>',
-      buttons: ['OK']
+      header: "Guardado exitoso",
+      message: "<hr>" + "<STRONG>La ubicación se guardó con éxito. </strong>",
+      buttons: ["OK"],
     });
 
     await alert.present();
   }
 
-
-
   async closeModal() {
-    // this.mapaDatosService.latitud = this.latitud;
-    // this.mapaDatosService.longitud = this.longitud;
-    // this.mapaDatosService.marcador_guardado = true;
-    // //console.log('la posicion del marcador en lat: '+this.mapaDatosService.latitud);
-    // //console.log('la posicion del marcador en lng: '+this.mapaDatosService.longitud);
-    // this.posicion = this.latitud.toString() +"|"+this.longitud.toString();
-    // await this.modalController.dismiss(this.posicion);
-    await this.guardar_marcador();
+    await this.saveMarker();
   }
-
 }
